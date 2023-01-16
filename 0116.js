2d16d0e97fa17656ea6c3020b671ae880107ee49
gyupp <kkb75281@gmail.com>
2023-01-16 19:12:14 +0900

    2023.01.16

---
+++ 0116.js
@@ -0,0 +1,25 @@
+// 01
+// let promise = new Promise(function(resolve, reject) {
+//     setTimeout(() => resolve(123), 1000);
+//     setTimeout(() => reject(new Error("에러에러")), 1000);
+// });
+
+// promise.then(result => console.log(result), error => console.log(error));
+
+
+
+
+// 02
+// function delay(ms) {
+//     // 여기에 코드 작성
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
+  
+// delay(3000).then(() => console.log('3초후 실행'));
+
+
+
+
+// 03
+
+  
